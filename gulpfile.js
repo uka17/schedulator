fe9ada82241b2c1811c872030875e92f97bd83e4
Nikolai Kozhekhov <ukaoneseven@gmail.com>
2021-11-23 21:21:41 +0300

    Gulp build improvement

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,39 @@
-const { src, dest } = require("gulp");
-const browserify = require("gulp-browserify");
-const minify = require("gulp-minify");
+"use strict";
 
-exports.default = function () {
-  return src("lib/schedulator.js")
-    .pipe(
-      browserify({
-        insertGlobals: true,
-        debug: true,
-      })
-    )
-    .pipe(minify())
-    .pipe(dest("dist"));
-};
+var browserify = require("browserify");
+var gulp = require("gulp");
+const { parallel } = require("gulp");
+var source = require("vinyl-source-stream");
+var buffer = require("vinyl-buffer");
+var uglify = require("gulp-uglify");
+
+function javascript() {
+  // set up the browserify instance on a task basis
+  var b = browserify({
+    entries: "lib/schedulator.js",
+    debug: true,
+  });
+
+  return b
+    .bundle()
+    .pipe(source("schedulator.js"))
+    .pipe(buffer())
+    .pipe(gulp.dest("./dist"));
+}
+
+function min() {
+  // set up the browserify instance on a task basis
+  var b = browserify({
+    entries: "lib/schedulator.js",
+    debug: true,
+  });
+
+  return b
+    .bundle()
+    .pipe(source("schedulator-min.js"))
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest("./dist"));
+}
+
+exports.build = parallel(javascript, min);
